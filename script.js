15986592060b48d49eef3bb1b0e6885bd6b09470
JAI-THE-GUY <146490870+JAI2011@users.noreply.github.com>
2025-04-25 18:25:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
+
+    const gravity = 0.5;
+    var groundLevel = 350;
+
+    const keys = {};
+
+    class Player {
+      
+      constructor(x, color, leftKey, rightKey, jumpKey) {
+        this.x = x;
+        this.y = groundLevel - 50;
+        this.width = 40;
+        this.height = 50;
+        this.color = color;
+        this.velX = 0;
+        this.velY = 0;
+        this.speed = 3;
+        this.jumpPower = -10;
+        this.onGround = false;
+        this.leftKey = leftKey;
+        this.rightKey = rightKey;
+        this.jumpKey = jumpKey;
+      }
+
+      update() {
+        
+
+        // Horizontal movement
+        if (keys[this.leftKey]) this.velX = -this.speed;
+        else if (keys[this.rightKey]) this.velX = this.speed;
+        else this.velX = 0;
+
+        // Jump
+        if (keys[this.jumpKey] && this.onGround) {
+          this.velY = this.jumpPower;
+          this.onGround = false;
+        }
+
+        // Gravity
+        this.velY += gravity;
+
+        // Position update
+        this.x += this.velX;
+        this.y += this.velY;
+
+        // Ground collision
+        if (this.y + this.height > groundLevel) {
+          this.y = groundLevel - this.height;
+          this.velY = 0;
+          this.onGround = true;
+        }
+
+        // Player collision with other player
+        
+
+        // Wall bounds
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.width > canvas.width) this.x = canvas.width - this.width;
+      }
+
+      draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
+
+    const player1 = new Player(100, "red", "a", "d", "w");
+    const player2 = new Player(1400, "blue", "ArrowLeft", "ArrowRight", "ArrowUp");
+
+    window.addEventListener("keydown", e => keys[e.key] = true);
+    window.addEventListener("keyup", e => keys[e.key] = false);
+
+    function drawGround() {
+      ctx.fillStyle = "#444";
+      ctx.fillRect(0, groundLevel, canvas.width, canvas.height - groundLevel);
+    }
+
+    function gameLoop() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      drawGround();
+
+      player1.update();
+      player2.update();
+
+      player1.draw();
+      player2.draw();
+
+      requestAnimationFrame(gameLoop);
+    }
+
+    gameLoop();
